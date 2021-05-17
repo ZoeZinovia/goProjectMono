349df0c98f8d9e3c0005a7f2ec443da307900989
Zoe Zinovia <53115380+ZoeZinovia@users.noreply.github.com>
2021-05-17 15:11:54 +0100

    m

--- pirMicroservicePublisher.go
+++ pirMicroservicePublisher.go
@@ -1,5 +1,88 @@
 package main
 
+// #cgo LDFLAGS: -lwiringPi
+// #include <wiringPi.h>
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <stdint.h>
+// #include <string.h>
+// #include <time.h>
+// #include <unistd.h>
+// #define MAX_TIMINGS	85
+// #define DHT_PIN		7	/* GPIO-4 */
+// int data[5] = { 0, 0, 0, 0, 0 };
+// double read_dht_data()
+// {
+//  clock_t begin = clock();
+//	wiringPiSetup();
+// 	uint8_t laststate	= HIGH;
+// 	uint8_t counter		= 0;
+// 	uint8_t j			= 0, i;
+// 	data[0] = data[1] = data[2] = data[3] = data[4] = 0;
+// 	/* pull pin down for 18 milliseconds */
+// 	pinMode( DHT_PIN, OUTPUT );
+// 	digitalWrite( DHT_PIN, LOW );
+// 	delay( 18 );
+// 	/* prepare to read the pin */
+// 	digitalWrite( DHT_PIN, HIGH);
+//  delayMicroseconds( 40 );
+// 	pinMode( DHT_PIN, INPUT );
+// 	/* detect change and read data */
+// 	for ( i = 0; i < MAX_TIMINGS; i++ )
+// 	{
+// 		counter = 0;
+// 		while ( digitalRead( DHT_PIN ) == laststate )
+// 		{
+// 			counter++;
+// 			delayMicroseconds( 2 );
+// 			if ( counter == 255 )
+// 				break;
+// 		}
+// 		laststate = digitalRead( DHT_PIN );
+// 		if ( counter == 255 ){
+// 			break;
+//		}
+// 		/* ignore first 3 transitions */
+// 		if ( (i >= 4) && (i % 2 == 0) )
+// 		{
+// 			/* shove each bit into the storage bytes */
+// 			data[j / 8] <<= 1;
+// 			if ( counter > 16 )
+// 				data[j / 8] |= 1;
+// 			j++;
+// 		}
+// 	}
+// 	/*
+// 	 * check we read 40 bits (8bit x 5 ) + verify checksum in the last byte
+// 	 * print it out if data is good
+// 	 */
+// 	if ( (j >= 40) &&
+// 	     (data[4] == ( (data[0] + data[1] + data[2] + data[3]) & 0xFF) ) )
+// 	{
+//		FILE *f = fopen("reading.txt", "w");
+// 	   	if (f == NULL)
+// 	  	{
+// 	   		printf("Error opening file!\n");
+// 	   		exit(1);
+// 	   	}
+//	   	fprintf(f, "%d,%d,%d,%d,%d", data[0], data[1], data[2], data[3], data[4]);
+//     	fclose(f);
+//		clock_t end = clock();
+//      double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+//		return time_spent;
+// 	} else  {
+//		FILE *f = fopen("reading.txt", "w");
+// 	   	if (f == NULL)
+// 	  	{
+// 	   		printf("Error opening file!\n");
+// 	   		exit(1);
+// 	   	}
+//	   	fprintf(f, "%d,%d,%d,%d,%d", data[0], data[1], data[2], data[3], data[4]);
+// 		fclose(f);
+//		return data[0];
+// 	}
+// }
+import "C"
 import (
 	"encoding/json"
 	"fmt"
